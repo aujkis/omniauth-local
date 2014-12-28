cc985129bda17105db639bde65d1afe37a762c7a
Austris Landmanis <austris@chevron.lv>
2014-12-28 16:41:13 +0200

    Hello

--- lib/omniauth/strategies/local.rb
+++ lib/omniauth/strategies/local.rb
@@ -3,8 +3,6 @@ module OmniAuth
     class Local
       include OmniAuth::Strategy
 
-      option :identifier, :email
-
       def request_phase
         redirect "/sign-in"
       end
@@ -15,16 +13,18 @@ module OmniAuth
       end
 
       def account
-        @account ||= Account.send("find_by_#{options[:identifier]}", identifier)
+        @account ||= Account.find_by(email: email)
+        # @account ||= Account.send("find_by_#{options[:identifier]}", identifier)
       end
 
       def identity
         @identity ||= Identity.find_by(strategy: :local, account_id: account.id) if account.present?
       end
 
-      def identifier
+      def email
         return nil unless request[:identity]['account_attributes']
-        request[:identity]['account_attributes'][options[:identifier].to_s].send(:to_s)
+        request[:identity]['account_attributes']['email'].send(:to_s)
+        # request[:identity]['account_attributes'][options[:identifier].to_s].send(:to_s)
       end
 
       def password
